3751179dd648337d1876539b92de44212984c5a8
GabrielSantosWK <gabriel.santos@wkconsultoria.com.br>
2022-03-10 06:58:23 -0300

    Implementacoes

---
+++ Models/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings.Models
+{
+    class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj is Pessoa) {
+                Pessoa objPessoa = (Pessoa)obj;
+                return objPessoa.Id == Id; 
+            }
+            return false;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Strings.Models;
+using System;
 using System.Text.RegularExpressions;
 
 namespace Strings
@@ -7,23 +8,37 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            //IntroducaoRegex();
+            IntroducaoEquals();
+        }
+        public static void IntroducaoEquals() {
+            var gui = new Pessoa() {Id = 1,Nome = "Guilherme" };
+            
+            var pedro = new Pessoa() {Id = 2, Nome = "Pedro jose" };
+
+            Console.WriteLine(pedro.Equals(2));
+            
+        }
+        public static void IntroducaoRegex() {
             var frase1 = "Me ligue em 1234-4568";
             var frase2 = "Meu numero é 1234-4568 me liga";
             var frase3 = "Me ligue em 1234-4568 esse é meu numero";
             var padrao1 = "[123456789][123456789][123456789][123456789]-[123456789][123456789][123456789][123456789]";
             var padrao2 = "[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]";
             var padrao3 = "[0-9]{4}-[0-9]{4}";
-            var padrao4 = "[0-9]{4,5}-[0-9]{4}";
-            var padrao5 = "[0-9]{4,5}-?[0-9]{4}";
-            Console.WriteLine(Regex.Match(frase1,padrao1));           
-            Console.WriteLine(Regex.Match(frase2, padrao2));            
+            //var padrao4 = "[0-9]{4,5}-[0-9]{4}";
+            //var padrao5 = "[0-9]{4,5}-?[0-9]{4}";
+            Console.WriteLine(Regex.Match(frase1, padrao1));
+            Console.WriteLine(Regex.Match(frase2, padrao2));
             Console.WriteLine(Regex.Match(frase3, padrao3));
 
             var url = "paginas?argumentos";
-            var _substring = url.Substring(url.IndexOf("?")+1);
+            var _substring = url.Substring(url.IndexOf("?") + 1);
             Console.WriteLine(_substring);
+
         }
 
-        
+
+
     }
 }
